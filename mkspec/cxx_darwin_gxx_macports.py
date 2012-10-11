11c090130abec1e8b50b6627bd8a823b5d2cb366
Jeppe Pihl <jpihl08@gmail.com>
2012-10-11 16:18:58 +0200

    moved mkspec files to external-waf-mkspec repository

--- mkspec/cxx_darwin_gxx_macports.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# encoding: utf-8
-
-def configure(conf):
-    """
-    We just select the platform default toolchain. And rely on Waf to detect it
-    """
-    conf.load('compiler_cxx')
