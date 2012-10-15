ba032ef125850f5f2737ae6e7f281e57ee711e34
Jeppe Pihl <jpihl08@gmail.com>
2012-10-15 15:20:33 +0200

    added filtering capabilities to mkspec

--- tools/wurf_cxx_mkspec.py
+++ tools/wurf_cxx_mkspec.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-import os, sys
+import os, sys, inspect
 
 from waflib import Utils
 from waflib import Context
 from waflib import Options
+from waflib import Errors
 
 from waflib.Configure import conf
 from waflib.Configure import ConfigurationContext
@@ -39,26 +40,56 @@ class ToolchainConfigurationContext(ConfigurationContext):
 
         super(ToolchainConfigurationContext, self).init_dirs()
 
-
-
 def options(opt):
     toolchain_opts = opt.add_option_group('mkspecs')
 
-    toolchain_opts.add_option('--cxx_mkspec', default = None,
-                              dest='cxx_mkspec',
-                              help="Select a specific cxx_mkspec "
-                                   "[default: %default]"
-                                   ", other example --cxx_mkspec=cxx_android_gxx")
+    toolchain_opts.add_option(
+        '--cxx_mkspec', default = None,
+        dest='cxx_mkspec',
+        help="Select a specific cxx_mkspec. "+
+             "Example: --cxx_mkspec=cxx_linux_x86-64_gxx4.6")
+
+    toolchain_opts.add_option(
+        '--cxx_mkspec-options', default = None,
+        dest='cxx_mkspec_options',
+        help="Some mkspec requires addtional options. Example: "+
+             "--cxx-mkspec-options=NDK_DIR=~/.android-standalone-ndk/gcc4.4/bin"
+        )
+
     opt.load("compiler_cxx")
 
-def configure(conf):
-    # Check to see if we should look for the mkspec in another path
 
+
+def configure(conf):
+    # Which mkspec should we use, by default, use the default one.
     mkspec = "cxx_default"
 
     if conf.options.cxx_mkspec:
         mkspec = conf.options.cxx_mkspec
 
-    cxx_mkspec_path = conf.env["BUNDLE_DEPENDENCIES"]["mkspec"]
+    #Check to see if the given mkspec is allowed in this project.
+    if filter and not filter(mkspec):
+        raise Errors.WafError(
+            msg="Unsupported mkspec, the mkspec must comply with:\n"+
+                 inspect.getsource(filter))
+
+    #Where should we look for the mkspec?
+
+    cxx_mkspec_path = "."
+    try:
+        cxx_mkspec_path = conf.env["BUNDLE_DEPENDENCIES"]["mkspec"]
+    except Exception, e:
+        # Just use the current directory then.
+        pass
+
+
     conf.msg('Setting cxx_mkspec path to:', cxx_mkspec_path)
+    conf.msg('Using the mkspec:', mkspec)
     conf.load(mkspec, cxx_mkspec_path)
+
+filter = None
+
+@conf
+def filter_mkspec(self, f):
+    global filter
+    filter = f
