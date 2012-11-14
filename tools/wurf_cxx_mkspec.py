f2bec899e9e0d4c9325f8e9a4e9cd252daefb8f3
Jeppe Pihl <jpihl08@gmail.com>
2012-11-14 15:41:20 +0100

    cleaned up and made sure that an error is shown if the mkspec platform is set twice

--- tools/wurf_cxx_mkspec.py
+++ tools/wurf_cxx_mkspec.py
@@ -20,11 +20,11 @@ class ToolchainConfigurationContext(ConfigurationContext):
     cmd='configure'
 
     def init_dirs(self):
-        # Waf calls this function to set the output dir.
+        # Waf calls this function to set the output directory.
         # Waf sets the output dir in the following order
         # 1) Check whether the -o option has been specified
         # 2) Check whether the wscript has an out varialble defined
-        # 3) Fallback and use the name of the lockfile
+        # 3) Fallback and use the name of the lock-file
         #
         # In order to not suprise anybody we will disallow the out variable
         # but allow our output dir to be overwritten by using the -o option
@@ -47,54 +47,65 @@ def options(opt):
         '--cxx-mkspec', default = None,
         dest='cxx_mkspec',
         help="Select a specific cxx_mkspec. "+
-             "Example: --cxx_mkspec=cxx_linux_x86-64_gxx4.6")
+             "Example: --cxx_mkspec=cxx_gxx46_arm_android")
 
     toolchain_opts.add_option(
         '--cxx-mkspec-options', default = None, action="append",
         dest='cxx_mkspec_options',
-        help="Some mkspec requires addtional options. Example: "+
-             "--cxx-mkspec-options=NDK_DIR=~/.android-standalone-ndk/gcc4.4/bin")
+        help="Some mkspec requires additional options, you can this option "
+             "multiple times. Example use: --cxx-mkspec-options="
+             "NDK_DIR=~/.android-standalone-ndk/gcc4.6/bin")
 
     opt.load("compiler_cxx")
 
-
-@conf
-def get_mkspec_option(conf, name):
-
-    if not name in conf.env['cxx_mkspec_options']:
-        conf.fatal('The %s options was not specified' % name)
-
-    return conf.env['cxx_mkspec_options'][name]
-
-
-@conf
-def set_mkspec_option(conf, name, value):
-    conf.env['cxx_mkspec_options'][name] = value
-
-
 # Allows us to catch queries for platforms that we do not yet support
+
 mkspec_platforms = ['windows','linux', 'android', 'mac']
+
+# If we ever need to do special things on specific platforms:
+"""
+mkspec_platform_specializations = { 'windows' : ['windows xp',
+                                                 'windows vista',
+                                                 'windows 7',
+                                                 'windows 8'],
+                                    'linux'   : ['ubuntu 12.04',
+                                                 'debian wheezy'],
+                                    'android' : ['android 2.3',
+                                                 'android 4.1'],
+                                    'mac'     : ['mac 10.8']
+                                   }
+"""
 
 @conf
 def get_mkspec_platform(conf):
-    return get_mkspec_option('MKSPEC_PLATFORM')
+    #If the MKSPEC_PLATFORM is not set, we auto detect it.
+    if conf.env['MKSPEC_PLATFORM'] == None:
+
+        platform = Utils.unversioned_sys_platform()
+        if platform == 'win32':
+            platform = 'windows'
+        elif platform == 'darwin':
+            platform = 'mac'
+        set_mkspec_platform(platform)
+    else:
+        return conf.env['MKSPEC_PLATFORM']
 
 @conf
 def set_mkspec_platform(conf, platform):
-    if not platform in mkspec_platforms:
-        conf.fatal("The mkspec platform %s is not supported"
-                   " supported is %s" % (platform, mkspec_platform))
+    if conf.env['MKSPEC_PLATFORM'] != []:
+        conf.fatal(("The mkspec platform could not be set to %s, as it was "
+                   "already set to %s.") %
+                   (platform, conf.env['MKSPEC_PLATFORM']))
 
-    conf.set_mkspec_option('MKSPEC_PLATFORM', platform)
+    if not platform in mkspec_platforms:
+        conf.fatal(("The mkspec platform %s is not supported"
+                   " supported is %s") % (platform, mkspec_platform))
+
+    conf.env['MKSPEC_PLATFORM'] = platform
 
 @conf
 def is_mkspec_platform(conf, platform):
-    if not platform in mkspec_platforms:
-        conf.fatal("The mkspec platform %s is not supported"
-                   " supported is %s" % (platform, mkspec_platform))
-
-    return conf.get_mkspec_platform == platform
-
+    return conf.get_mkspec_platform() == platform
 
 def read_options(conf):
     conf.env["cxx_mkspec_options"] = {}
@@ -103,9 +114,9 @@ def read_options(conf):
             try:
                 key, value = option.split('=')
             except Exception, e:
-                conf.fatal("cxx-mkspec-options has to have the format 'KEY=VALUE'"
-                           ", you specified %r,"
-                           "which resulted in Error:'%s'" %(option, e))
+                conf.fatal("cxx-mkspec-options has to have the format"
+                           "'KEY=VALUE', you specified %r, which resulted in"
+                           "Error:'%s'" % (option, e))
             conf.env["cxx_mkspec_options"][key] = value
 
 def configure(conf):
@@ -117,12 +128,10 @@ def configure(conf):
 
     #Check to see if the given mkspec is allowed in this project.
     if filter and not filter(mkspec):
-        raise Errors.WafError(
-            msg="Unsupported mkspec, the mkspec must comply with:\n"+
-            inspect.getsource(filter))
+        raise Errors.WafError(msg=("Unsupported mkspec, the mkspec must comply"
+                                   "with:%s") % inspect.getsource(filter))
 
     #Where should we look for the mkspec?
-
     cxx_mkspec_path = "."
     try:
         cxx_mkspec_path = conf.env["BUNDLE_DEPENDENCIES"]["mkspec"]
@@ -133,7 +142,6 @@ def configure(conf):
     read_options(conf)
 
     conf.load(mkspec, cxx_mkspec_path)
-    #conf.msg('Setting cxx_mkspec path to:', cxx_mkspec_path)
     conf.msg('Using the mkspec:', mkspec)
 
 filter = None
