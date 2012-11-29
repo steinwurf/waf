bc80176fb725b11aba6a26fd6c88d8a05d54cfb6
Jeppe Pihl <jpihl08@gmail.com>
2012-11-29 19:19:59 +0100

    updated wurf tools

--- tools/wurf_tools.py
+++ tools/wurf_tools.py
@@ -5,45 +5,63 @@ import os
 from waflib.Configure import conf
 
 def options(opt):
-    
     tool_opts = opt.add_option_group('external tools')
 
     tool_opts.add_option(
         '--options', default = None, action="append",
         dest='tool_options',
-        help="Some external Waf tools requires additional options, you can this option "
-             "multiple times. Example use: --options="
-             "NDK_DIR=~/.android-standalone-ndk/gcc4.6/bin. See more information about "
-             "the external tool options here: http:/sdkfjdsl")
+        help="Some external Waf tools requires additional options, you can "
+             "this option multiple times. See more information about the "
+             "external tool options here: http:/sdkfjdsl")
 
 
-def read_tool_options(conf):
-    conf.env["tool_options"] = {}
-    if conf.options.tool_options:
-        for options in conf.options.tool_options:
+def parse_options(options_string):
+    result = {}
+    if options_string:
+        for options in options_string:
             for option in options.split(','):
                 try:
                     key, value = option.split('=')
-                except Exception, e:
-                    conf.fatal("tool-options has to have the format"
-                               "'KEY=VALUE', you specified %r, which resulted in"
-                               "Error:'%s'" % (option, e))
-                conf.env["tool_options"][key] = value
+                    result[key] = value
+                except ValueError, e:
+                    result[option] = True
+    return result
+
+def _read_tool_options(conf):
+    conf.env["tool_options"] = parse_options(conf.options.tool_options)
+
 
 def configure(conf):
-    read_tool_options(conf)
+    _read_tool_options(conf)
+
+def check_for_duplicate(conf):
+    options = parse_options(conf.options.tool_options)
+    for option in options:
+        if (option in conf.env['tool_options'] and
+            conf.env['tool_options'][option] != options[option]):
+            conf.fatal("Redefined option '%s' from %s to '%s', "
+                       "re-run configure, to override the old value."
+                       % (option,
+                          conf.env['tool_options'][option],
+                          options[option]))
 
 @conf
 def get_tool_option(conf, option):
-    if not option in conf.env['tool_options']:
-        conf.fatal('No tool option %s, you can specify tool options as: '
-                   './waf configure --options=KEY=VALUE')
+    check_for_duplicate(conf)
+    value = None
+    if option in conf.env.tool_options:
+        value = conf.env.tool_options[option]
+    elif option in conf.options.tool_options:
+        value = conf.options.tool_options[option]
     else:
-        return conf.env['tool_options'][option]
+        conf.fatal('No tool option %s, you can specify tool options as: '
+                   './waf configure --options=KEY=VALUE' % option)
+    return value
 
 @conf
 def has_tool_option(conf, option):
-    return option in conf.env['tool_options']
+    check_for_duplicate(conf)
+    return option in conf.env.tool_options or option in parse_options(conf.options.tool_options)
 
 @conf
 def load_external_tool(conf, category, name):
@@ -55,6 +73,5 @@ def load_external_tool(conf, category, name):
     # Get the path and load the tool
     path = conf.dependency_path('waf-tools')
 
-    conf.load(name, os.path.join(path, category)) 
-
+    conf.load(name, os.path.join(path, category))
 
