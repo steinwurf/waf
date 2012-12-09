61316eb3448b05fd01ee4d0c2a7e195b5d8185bb
Morten V. Pedersen <morten@mortenvp.dk>
2012-12-09 14:16:24 +0100

    Adding the configure output tool

---
+++ tools/wurf_configure_output.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+# This tools overrides the output directory / build
+# directory of waf. This is mainly to support cross-
+# compilation into separated output directories.
+
+import os, sys, inspect, ast
+
+from waflib import Utils
+from waflib import Context
+from waflib import Options
+from waflib import Errors
+
+from waflib.Configure import conf
+from waflib.Configure import ConfigurationContext
+
+
+class ToolchainConfigurationContext(ConfigurationContext):
+    '''configures the project'''
+    cmd='configure'
+
+    def init_dirs(self):
+        # Waf calls this function to set the output directory.
+        # Waf sets the output dir in the following order
+        # 1) Check whether the -o option has been specified
+        # 2) Check whether the wscript has an out varialble defined
+        # 3) Fallback and use the name of the lock-file
+        #
+        # In order to not suprise anybody we will disallow the out variable
+        # but allow our output dir to be overwritten by using the -o option
+
+        assert(getattr(Context.g_module,Context.OUT,None) == None)
+
+        if not Options.options.out:
+
+            if self.has_tool_option('cxx_mkspec'):
+                mkspec = self.get_tool_option('cxx_mkspec')
+                self.out_dir = os.path.join("build", mkspec)
+            else:
+                build_platform = Utils.unversioned_sys_platform()
+                self.out_dir = os.path.join("build", build_platform)
+
+        super(ToolchainConfigurationContext, self).init_dirs()
+
+
