0ce4041a5b3efa2497c825317e85250b21e99692
Peter Vingelmann <peter@steinwurf.com>
2013-07-17 15:52:34 +0200

    fixing wurf_standalone on windows

--- tools/wurf_standalone.py
+++ tools/wurf_standalone.py
@@ -7,11 +7,11 @@ from waflib import Options
 def options(opt):
 
     opt.add_option('--standalone_archive', action='store', default = None,
-                   help = 'Name of the standalone archive '
+                   help = 'Name of the standalone archive, '
                           'e.g. test_archive')
 
     opt.add_option('--standalone_algo', action='store', default = 'zip',
-                   help = 'valid algo types are tar.bz2, tar.gz or zip')
+                   help = 'valid algo types are zip, tar.bz2 and tar.gz')
 
 
 class Standalone(Dist):
@@ -26,14 +26,15 @@ class Standalone(Dist):
 
         self.algo = Options.options.standalone_algo
 
-    def get_excl(self):
-        excl = super(Standalone, self).get_excl()
-        excl += 'build **/.git **/.gitignore **/*~ **/*.pyc .lock* *.bat ' \
+    def get_files(self):
+        excludes = Dist.get_excl(self)
+        excludes += ' build **/.git **/.gitignore **/*~ **/*.pyc .lock* *.bat ' \
                 'waf-* .waf-* *.zip *.tar.bz2 *.tar.gz '\
+                'VSProjects *.sln *.sdf *.suo '\
                 'bundle_dependencies/*/master/*'
 
-        return excl
+        return self.base_path.ant_glob('**/*', dir = True, excl = excludes.split(' '))
 
 def standalone(ctx):
-        '''makes a tarball for redistributing the sources'''
+        """Makes an archive for redistributing the sources"""
         pass
