16e2c5aa25aa63d5f5d41b9728e8581abe3151c9
Jeppe Pihl <jpihl08@gmail.com>
2014-05-20 17:08:11 +0200

    Rewrote ResolveGitMajorVersion to a more generic ResolveVersion

--- test/python/test_dependency_resolve.py
+++ test/python/test_dependency_resolve.py
@@ -5,7 +5,7 @@ sys.path.append('../../tools')
 sys.path.append('../../python-semver')
 
 import unittest
-import dependency_resolve as dr
+import wurf_dependency_resolve as dr
 import mock
 import os
 
@@ -13,58 +13,74 @@ import os
 class TestResolveGitMajorVersion(unittest.TestCase):
 
     def test_select_tag(self):
-        tags = ['2.3.4', '1.0.0', '1.3.5', '1.0.1', '2.0.0', '1.1.2']
+        tags = ['2.3.4', '1.0.0', '1.3.5', '1.0.1', '2.0.0', '1.1.2', '1.1.1']
 
         # Major version 1
-        resolver = dr.ResolveGitMajorVersion(name="test",
-                                             git_repository="git://dummy.git",
-                                             major_version=1)
+        resolver = dr.ResolveVersion(name="test",
+                                     git_repository="dummy.git",
+                                     major=1)
 
         tag = resolver.select_tag(tags)
         self.assertEqual(tag, '1.3.5')
 
         # Major version 2
-        resolver = dr.ResolveGitMajorVersion(name="test",
-                                             git_repository="git://dummy.git",
-                                             major_version=2)
+        resolver = dr.ResolveVersion(name="test",
+                                     git_repository="dummy.git",
+                                     major=2)
+
+        tag = resolver.select_tag(tags)
+        self.assertEqual(tag, '2.3.4')
+
+        # Major minor version 1
+        resolver = dr.ResolveVersion(name="test",
+                                     git_repository="dummy.git",
+                                     major=1,
+                                     minor=1)
+
+        tag = resolver.select_tag(tags)
+        self.assertEqual(tag, '1.1.2')
+
+        # Major minor patch version 1
+        resolver = dr.ResolveVersion(name="test",
+                                     git_repository="dummy.git",
+                                     major=1,
+                                     minor=1,
+                                     patch=1)
+
+        tag = resolver.select_tag(tags)
+        self.assertEqual(tag, '1.1.1')
+
+        # Major version 2
+        resolver = dr.ResolveVersion(name="test",
+                                     git_repository="dummy.git",
+                                     major=2)
 
         tag = resolver.select_tag(tags)
         self.assertEqual(tag, '2.3.4')
 
     def test_resolve(self):
-        resolver = dr.ResolveGitMajorVersion(name="test",
-                                             git_repository="git://dummy.git",
-                                             major_version=1)
+        resolver = dr.ResolveVersion(name="test",
+                                     git_repository="dummy.git",
+                                     major=1)
 
         m = mock.Mock()
         m.git_tags = mock.Mock(return_value=['1.0.0', '1.2.3', '1.2.0'])
-
         repo_dir = os.path.abspath(os.path.expanduser('~/here'))
 
-        resolver.resolve(m, repo_dir)
+        resolver.resolve(
+            ctx=m,
+            path=repo_dir,
+            use_checkout=None,
+            protocol_handler='git://')
 
         # Check function calls
         self.assertEqual(m.method_calls[0], (
-            'git_clone', ('git://dummy.git', repo_dir + '/test-master'),
-            {'cwd': repo_dir}))
+            'git_clone', ('git://dummy.git', repo_dir + '/test-9199d9/master'),
+            {'cwd': repo_dir + '/test-9199d9'}))
 
-        self.assertEqual(m.method_calls[1], (
-            'git_pull', (),
-            {'cwd': repo_dir + '/test-master'}))
-
-        # We don't see the call to git_tags() here since we mocked it
-        # out above
-
-        self.assertEqual(m.method_calls[2], (
-            'git_local_clone', (repo_dir + '/test-master',
-                                repo_dir + '/test-1.2.3'),
-            {'cwd': repo_dir}))
-
-        self.assertEqual(m.method_calls[3], (
-            'git_checkout', ('1.2.3',),
-            {'cwd': repo_dir + '/test-1.2.3'}))
-
-        # print m.method_calls
+        self.assertEqual(
+            m.method_calls[1],
+            ('git_pull', (), {'cwd': repo_dir + '/test-9199d9/master'}))
 
 if __name__ == '__main__':
     unittest.main()
