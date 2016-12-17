963f14dc41bf47b0a7b583d8b26953a62dd0b411
Morten V. Pedersen <morten@mortenvp.com>
2016-12-17 22:47:37 +0100

    Working on it

--- test/test_add_dependency/test_add_dependency.py
+++ test/test_add_dependency/test_add_dependency.py
@@ -1,15 +1,24 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
+import shutilwhich
+
 def test_add_dependency(test_directory):
     """ Integration testing of adding a dependency."""
 
-    test_directory.copy_file('build/waf')
-    test_directory.copy_file('test/test_add_dependency/wscript')
+    main_dir = test_directory.mkdir('main')
+
+    main_dir.copy_file('build/waf')
+    main_dir.copy_file('test/test_add_dependency/wscript')
 
     # The bundle_dependencies directory is the default, so when we do
     # configure without any arguments dependencies are expected to be
     # placed there.
+    
+    git_binary = shutilwhich.which('git')
+    
+    #libfoo_repo = test
+    
     #bundle_directory = test_directory.mkdir('bundle_dependencies')
 
 
@@ -18,37 +27,37 @@ def test_add_dependency(test_directory):
     ##libfoo_directory = bundle_directory.mkdir('libbar-h4sh')
     #libfoo_directory.copy_dir('test/test_add_dependency/libbar')
 
-    r = test_directory.run('python', 'waf', 'configure', '-v')
+    r = main_dir.run('python', 'waf', 'configure', '-v')
 
     assert r.returncode == 0, str(r)
 
-    r = test_directory.run('python', 'waf', 'build', '-v')
+    r = main_dir.run('python', 'waf', 'build', '-v')
 
     assert r.returncode == 0, str(r)
 
-    r = test_directory.run('python', 'waf', 'configure', '-v', '--waf-use-checkout=waf-1.9.4')
-
-    assert r.returncode == 0, str(r)
-
-    r = test_directory.run('python', 'waf', 'build', '-v')
-
-    assert r.returncode == 0, str(r)
-
-    r = test_directory.run('python', 'waf', 'configure', '-v', '--waf-path=/tmp')
-
-    assert r.returncode == 0, str(r)
-
-    r = test_directory.run('python', 'waf', 'build', '-v')
-
-    assert r.returncode == 0, str(r)
-
-    r = test_directory.run('python', 'waf', 'configure', '-v', '--bundle-path=okidoki')
-
-    assert r.returncode == 0, str(r)
-
-    r = test_directory.run('python', 'waf', 'build', '-v')
-
-    assert r.returncode == 0, str(r)
-
+    # r = test_directory.run('python', 'waf', 'configure', '-v', '--waf-use-checkout=waf-1.9.4')
+    # 
+    # assert r.returncode == 0, str(r)
+    # 
+    # r = test_directory.run('python', 'waf', 'build', '-v')
+    # 
+    # assert r.returncode == 0, str(r)
+    # 
+    # r = test_directory.run('python', 'waf', 'configure', '-v', '--waf-path=/tmp')
+    # 
+    # assert r.returncode == 0, str(r)
+    # 
+    # r = test_directory.run('python', 'waf', 'build', '-v')
+    # 
+    # assert r.returncode == 0, str(r)
+    # 
+    # r = test_directory.run('python', 'waf', 'configure', '-v', '--bundle-path=okidoki')
+    # 
+    # assert r.returncode == 0, str(r)
+    # 
+    # r = test_directory.run('python', 'waf', 'build', '-v')
+    # 
+    # assert r.returncode == 0, str(r)
+    # 
 
     print(str(r))
