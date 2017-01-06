b9c50a5678f664229558f9bd9e165ea22449470b
Morten V. Pedersen <morten@mortenvp.com>
2017-01-06 17:36:22 +0100

    working on it...

---
+++ src/wurf/wurf_dependency.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+# encoding: utf-8
+
+class WurfDependency(dict):
+
+    def __getattr__(self, name):
+        if name in self:
+            return self[name]
+        else:
+            raise AttributeError("No such attribute: " + name)
+
+    def __setattr__(self, name, value):
+        self[name] = value
+
+    def __delattr__(self, name):
+        if name in self:
+            del self[name]
+        else:
+            raise AttributeError("No such attribute: " + name)
---
+++ src/wurf/wurf_error.py
@@ -0,0 +1,6 @@
+#! /usr/bin/env python
+# encoding: utf-8
+
+class WurfError(Exception):
+    """Basic exception for errors raised by wurf tools"""
+    pass
