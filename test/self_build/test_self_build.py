4757d452d0622c533f519b6c13655da1c6dec01c
Peter Vingelmann <petya@master.com>
2017-02-10 21:55:17 +0100

    Move root wscript to implement the self_build test

---
+++ test/self_build/test_self_build.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import os
+
+def test_self_build(test_directory):
+    root = test_directory
+
+    # Note: waf will call "git config --get remote.origin.url" in this folder,
+    # so "git init" is required to test the default behavior (https resolver)
+    root.run('git', 'init')
+
+    src_dir = root.mkdir('src')
+    src_dir.copy_dir(directory='src/wurf')
+    root.copy_file('test/self_build/wscript')
+    root.copy_file('build/waf')
+
+    # TODO: Remove this (waf should implicitly create the folder that is
+    # specified by the "bundle_path" option)
+    root.mkdir('bundle_dependencies')
+
+    r = root.run('python', 'waf', 'configure')
+
+    assert r.returncode == 0
+    assert r.stdout.match('*finished successfully*')
+
+    r = root.run('python', 'waf', 'build')
+
+    assert r.returncode == 0
+
+    waf_path = os.path.join(root.path(), 'build', 'waf')
+
+    assert os.path.isfile(waf_path)
