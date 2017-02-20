6c669681035b5a3891d4f1301a080b4afc559272
Peter Vingelmann <petya@master.com>
2017-02-21 00:18:18 +0100

    Use pre_recurse in WafBuildContext to call build() in all dependencies before the main build()

--- src/wurf/waf_build_context.py
+++ src/wurf/waf_build_context.py
@@ -6,18 +6,12 @@ from waflib.Build import BuildContext
 
 class WafBuildContext(BuildContext):
 
-    def pre_build(self):
-        # Call build in all dependencies.
-        #
-        # Note, why is this placed here? The BuildContext in waf will call this
-        # function right after recursing into the main wscript. The purpose of
-        # it seems to be to allow the user to hook up some user-defined functions
-        # right before starting a build.
-        #
-        # https://github.com/waf-project/waf/blob/master/waflib/Build.py#L269
-        #
-        #  Which is more or less what we want to do. Here we recurse into to the
-        #  dependencies build functions allowing them to also hook up
-        #  user-defined build functions.
-        self.recurse_dependencies()
-        super(WafBuildContext, self).pre_build()
+    def pre_recurse(self, node):
+
+        super(WafBuildContext, self).pre_recurse(node)
+
+        # Call build() in all dependencies before executing build()
+        # in the top-level wscript: this allows us to define all build tasks
+        # from the dependencies before reaching the main project
+        if self.is_toplevel():
+            self.recurse_dependencies()
