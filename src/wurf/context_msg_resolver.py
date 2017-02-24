1e2c1fd0b7d361605895026d14bf078722578dc4
Peter Vingelmann <petya@master.com>
2017-02-25 00:06:33 +0100

    Improve messages and paths in ContextMsgResolver

--- src/wurf/context_msg_resolver.py
+++ src/wurf/context_msg_resolver.py
@@ -24,10 +24,12 @@ class ContextMsgResolver(object):
 
         :return: The path as a string.
         """
-        
 
-        self.ctx.start_msg('{} {}'.format(
-            self.dependency.resolver_method, self.dependency.name))
+        start_msg = '{} "{}"'.format(
+            self.dependency.resolver_action, self.dependency.name)
+        if self.dependency.resolver_method:
+            start_msg += ' ({})'.format(self.dependency.resolver_method)
+        self.ctx.start_msg(start_msg)
 
         path = self.resolver.resolve()
 
@@ -37,10 +39,16 @@ class ContextMsgResolver(object):
             # print the status message and continue
             self.ctx.end_msg('Unavailable', color='RED')
         else:
-            
+
             if self.dependency.is_symlink:
+                # We print the symlink path as a relative path if it is
+                # inside the project folder
+                symlink_path = path
+                symlink_node = self.ctx.root.find_dir(path)
+                if symlink_node.is_child_of(self.ctx.srcnode):
+                    symlink_path = symlink_node.path_from(self.ctx.srcnode)
                 real_path = self.dependency.real_path
-                self.ctx.end_msg("{} => {}".format(path, real_path))
+                self.ctx.end_msg("{} => {}".format(symlink_path, real_path))
             else:
                 self.ctx.end_msg(path)
 
