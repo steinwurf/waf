8619dc7cea71a58c2b7ca9d32abf00e846ce2379
Morten V. Pedersen <morten@mortenvp.com>
2017-02-24 10:05:19 +0100

    Rename option to attribute and update docs

--- src/wurf/dependency.py
+++ src/wurf/dependency.py
@@ -41,15 +41,15 @@ class Dependency(object):
             # Access the sha1 attribute
             print("Dependency hash {}".format(dependency.sha1))
 
-        Additional attributes may be added to the dependency during resolve. 
-        However, they cannot overwrite the existing attributes created at 
+        Additional attributes may be added to the dependency during resolve.
+        However, they cannot overwrite the existing attributes created at
         construction time.
-        
-        So in the small example from before all the following attributes are 
-        read-only: name, recurse, optional, resolver, method, checkout, 
+
+        So in the small example from before all the following attributes are
+        read-only: name, recurse, optional, resolver, method, checkout,
         sources and sha1.
-        
-        Any other attributes can be added an modified as needed.
+
+        Any other attributes can be added and modified as needed.
 
         :param kwargs: Keyword arguments containing options for the dependency.
         """
@@ -59,84 +59,88 @@ class Dependency(object):
         s = json.dumps(kwargs, sort_keys=True)
         sha1 = hashlib.sha1(s.encode('utf-8')).hexdigest()
 
-        # kwargs is a dict, we add it as an instance attribute. 
+        # kwargs is a dict, we add it as an instance attribute.
         object.__setattr__(self, 'read_only', kwargs)
         self.read_only['sha1'] = sha1
 
         # For some operations like, storing a dependency in a dict as the key
-        # it is needed that the dependency is hashable (returned as an integer). 
-        # This is implemented in the __hash__() member function, which will 
+        # it is needed that the dependency is hashable (returned as an integer).
+        # This is implemented in the __hash__() member function, which will
         # compute the hash from the sha1 string. We will store this value to
         # avoid computing it over and over.
-        # 
+        #
         # A good question here, is why do we store both the sha1 and another
         # integer hash?
-        # 
-        # The sha1 is stored in config files describing a dependency, so it 
+        #
+        # The sha1 is stored in config files describing a dependency, so it
         # has to be stable. In the sense that if the config was written using
         # e.g. python2.7 and then read under python3.3 the sha1 must stay the
-        # same for a given input. 
-        # 
+        # same for a given input.
+        #
         # This is guarenteed by the SHA1 algorithm.
-        # 
-        # This guarentee does not seem (we are just not sure) to be implemented 
-        # for the in-built hash() function. Which could change it's 
-        # implementation for e.g. hashing strings between interpreter versions. 
-        # 
-        # However, we cannot rely only on the SHA1 as it is stored as a string 
-        # which means it cannot be returned from the __hash__ function (it 
+        #
+        # This guarentee does not seem (we are just not sure) to be implemented
+        # for the in-built hash() function. Which could change it's
+        # implementation for e.g. hashing strings between interpreter versions.
+        #
+        # However, we cannot rely only on the SHA1 as it is stored as a string
+        # which means it cannot be returned from the __hash__ function (it
         # requires an integer). The simplest solution therefore seemd to be:
-        # 
-        # 1. Use the stable / consistent SHA1 hash for describing a dependency 
+        #
+        # 1. Use the stable / consistent SHA1 hash for describing a dependency
         #    in external formats such as files
-        # 2. Use the Python hash() function to generate a integer hash for 
+        # 2. Use the Python hash() function to generate a integer hash for
         #    run-time usage (i.e. within the same interpreter session).
-        #super(Dependency, self).__setattr__('hash', None)
+
         self.read_only['hash'] = None
-        
+
         # Resolver attributes (modifiable)
         object.__setattr__(self, 'read_write', dict())
-        
-        
-    def __getattr__(self, option):
-        """ Return the value corresponding to the option.
 
-        :param option: The name of the option to return as a string.
-        :return: The option value.
+    def __getattr__(self, attribute):
+        """ Return the value corresponding to the attribute.
+
+        :param attribute: The name of the attribute to return as a string.
+        :return: The attribute value, if the attribute does not exist
+            return None
         """
-        if option in self.read_only:
-            return self.read_only[option]
+        if attribute in self.read_only:
+            return self.read_only[attribute]
 
-        elif option in self.read_write:
-            return self.read_write[option]
-        
+        elif attribute in self.read_write:
+            return self.read_write[attribute]
+
         else:
             return None
 
+    def __setattr__(self, attribute, value):
+        """ Sets a dependency attribute.
 
-    def __setattr__(self, name, value): 
-        """ Sets a resolver attribute."""
-
-        if name in self.read_only:
-            raise AttributeError("Attribute {} read-only.".format(name))
-        else:
-            self.read_write[name] = value   
-
-    def __contains__(self, option):
-        """ Checks if the option is available.
-        
-        :return: True if the option is available otherwise False
+        :param attribute: The name of the attribute as a string
+        :param value: The value of the attribute
         """
-        return (option in self.read_only) or (option in self.read_write)
+
+        if attribute in self.read_only:
+            raise AttributeError("Attribute {} read-only.".format(attribute))
+        else:
+            self.read_write[attribute] = value
+
+    def __contains__(self, attribute):
+        """ Checks if the attribute is available.
+
+        :return: True if the attribute is available otherwise False
+        """
+        return (attribute in self.read_only) or (attribute in self.read_write)
 
     def __str__(self):
-        return "Dependency\ninfo: {}\nresolver info: {}".format(
+        """ :return: String representation of the dependency. """
+        return "Dependency\nread only: {}\nread write: {}".format(
             self.read_only, self.read_write)
 
     def __hash__(self):
         """ :return: Integer hash value for the dependency. """
-        
+
         if not self.read_only['hash']:
             self.read_only['hash'] = hash(self.read_only['sha1'])
-        
+
         return self.read_only['hash']
