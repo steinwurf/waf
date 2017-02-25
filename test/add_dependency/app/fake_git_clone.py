8fdf61c31bab69cea9e62fe7869790602cc373ea
Morten V. Pedersen <morten@mortenvp.com>
2017-02-25 21:43:09 +0100

    Minor tweaks to the mappings

--- test/add_dependency/app/fake_git_clone.py
+++ test/add_dependency/app/fake_git_clone.py
@@ -13,14 +13,40 @@ class CloneError(Error):
 
 class FakeGitClone(Git):
 
-    def __init__(self, git_binary, ctx):
+    def __init__(self, git_binary, ctx, clone_path):
+        """ Create a new instance.
+
+        :param git_binary: The git executable
+        :param ctx: A Waf context instance
+        :param clone_path: A dict containg a mapping or git repository URL and
+            location on the file system.
+
+            The URL is the key and the file-system path is the value.
+
+            Example:
+
+                clone_path = {'foo.git': '/tmp/libfoo',
+                              'bar.git': '/tmp/libbar'}
+
+            Depending on the unit test you can provide more of the URL if e.g.
+            certain URLs should fail.
+
+            Example:
+
+                clone_path = {'github.com/acme-corp/foo.git': 'libfoo',
+                              'gitlab.com/acme-corp/bar.git': 'libbar'}
+
+            In this case we have no mapping for e.g. bitbucket so all those
+            URLs will raise a CloneError (simulating a mirror which is down).
+
+        """
         super(FakeGitClone, self).__init__(git_binary=git_binary, ctx=ctx)
 
         # This is the directory on the file system which contains the
         # test libraries. We use the url in the clone(...) function
         # to find the library needed
 
-        self.git_dir = ctx.path.parent.find_node('git_dir')
+        #
 
         # This mapping is use when performing a "fake" git clone.
         #
@@ -28,23 +54,23 @@ class FakeGitClone(Git):
         # under the 'git_dir' folder using the following mapping.
         # For the a given git URL the value in the dict below gives
         # the directory name in the 'git_dir'.
-        self.libraries = {'github.com/acme-corp/foo.git': 'libfoo',
-                     'gitlab.com/acme-corp/bar.git': 'libbar',
-                     'gitlab.com/acme/baz.git': 'libbaz' }
+        # self.libraries = {'github.com/acme-corp/foo.git': 'libfoo',
+        #                   'gitlab.com/acme-corp/bar.git': 'libbar',
+        #                   'gitlab.com/acme/baz.git': 'libbaz' }
+        self.clone_path = clone_path
 
     def clone(self, repository, directory, cwd):
 
         dst_directory = os.path.join(cwd, directory)
 
-        for lib_repository, lib_name in self.libraries.items():
+        for lib_repository, lib_directory in self.clone_path.items():
             if repository.endswith(lib_repository):
-
-                lib_directory = self.git_dir.find_node(lib_name).abspath()
 
                 shutil.copytree(src=lib_directory, dst=dst_directory,
                     symlinks=True)
 
-                assert os.path.isdir(dst_directory), "We should have a valid path here!"
+                assert os.path.isdir(dst_directory), "We should have a valid "\
+                    "path here!"
                 return
 
         else:
@@ -54,5 +80,7 @@ class FakeGitClone(Git):
     def build(registry):
         git_binary = registry.require('git_binary')
         ctx = registry.require('ctx')
+        clone_path = registry.require('clone_path')
 
-        return FakeGitClone(git_binary=git_binary, ctx=ctx)
+        return FakeGitClone(git_binary=git_binary, ctx=ctx,
+            clone_path=clone_path)
