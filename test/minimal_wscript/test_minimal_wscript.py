6742faca897bfde87f1f3edddd95bf35656bde1e
Peter Vingelmann <petya@master.com>
2017-03-07 23:43:58 +0100

    Make configure optional in top-level wscript + Rename no_resolve_wscript test to minimal_wscript

--- test/minimal_wscript/test_minimal_wscript.py
+++ test/minimal_wscript/test_minimal_wscript.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-def test_empty_wscript(test_directory):
-    test_directory.copy_file('test/no_resolve_wscript/wscript')
+def test_minimal_wscript(test_directory):
+    test_directory.copy_file('test/minimal_wscript/wscript')
     test_directory.copy_file('build/waf')
 
+    # The wscript has no "configure" function, but this step should not fail
     r = test_directory.run('python', 'waf', 'configure')
 
     assert r.returncode == 0
