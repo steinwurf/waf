a72898e739674b57d90d76def944448f185be5d0
Morten V. Pedersen <morten@mortenvp.com>
2017-04-17 14:44:26 +0200

    Working on it

---
+++ test/http_dependency/app/main.cpp
@@ -0,0 +1,15 @@
+// Copyright (c) Steinwurf ApS 2016.
+// All Rights Reserved
+//
+// Distributed under the "BSD License". See the accompanying LICENSE.rst file.
+
+#include <iostream>
+
+//#include <pretty-print>
+
+int main()
+{
+    std::cout << "app" << std::endl;
+
+    return 0;
+}
