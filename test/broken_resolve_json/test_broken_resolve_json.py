7c122f21df9c3c8c3b90cfed24faaba951bcf41f
Morten V. Pedersen <morten@mortenvp.com>
2017-10-02 13:53:51 +0200

    Gracefully handle error

--- test/broken_resolve_json/test_broken_resolve_json.py
+++ test/broken_resolve_json/test_broken_resolve_json.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
+import pytest
+from pytest_testdirectory.runresulterror import RunResultError
+
 
 def test_broken_resolve_json(testdirectory):
     testdirectory.copy_file('test/broken_resolve_json/wscript')
     testdirectory.copy_file('test/broken_resolve_json/resolve.json')
     testdirectory.copy_file('build/waf')
 
-    # The wscript has no "configure" function, but this step should not fail
-    r = testdirectory.run('python', 'waf', 'configure')
+    with pytest.raises(RunResultError) as excinfo:
+        testdirectory.run('python', 'waf', 'configure')
 
-    assert r.returncode == 0
-    assert r.stdout.match('*finished successfully*')
+    r = excinfo.value.runresult
 
-    r = testdirectory.run('python', 'waf', 'build')
-
-    assert r.returncode == 0
+    assert r.returncode != 0
+    assert r.stderr.match('Error in load dependencies (resolve.json)*')
