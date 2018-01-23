86d95082e872d0a7939aeb5bac15c55a5ed88157
Morten V. Pedersen <morten@mortenvp.com>
2018-01-23 15:15:24 +0100

    Adding test

---
+++ test/clang_compilation_database/main.cpp
@@ -0,0 +1,12 @@
+// Copyright (c) 2016 Steinwurf ApS
+// All Rights Reserved
+//
+// Distributed under the "BSD License". See the accompanying LICENSE.rst file.
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "hello world\n";
+    return 0;
+}
