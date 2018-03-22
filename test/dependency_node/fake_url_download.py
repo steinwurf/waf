4e032ab472e99ea90e584a243878c60f983982dd
Morten V. Pedersen <morten@mortenvp.com>
2018-03-22 14:07:05 +0100

    Working on it..

---
+++ test/dependency_node/fake_url_download.py
@@ -0,0 +1,23 @@
+import os
+
+
+class FakeUrlDownload(object):
+
+    def __init__(self):
+        pass
+
+    def download(self, cwd, source, filename):
+
+        assert not filename
+        filename = 'lib.cpp'
+
+        file_path = os.path.join(cwd, filename)
+
+        with open(file_path, 'w') as the_file:
+            the_file.write('Hello\n')
+
+        return file_path
+
+    @staticmethod
+    def build(registry):
+        return FakeUrlDownload()
