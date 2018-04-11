1931e2d536fa19718be4b0b0c671983effeacc24
Morten V. Pedersen <morten@mortenvp.com>
2018-04-11 15:34:02 +0200

    Fix pycodestyle issue

--- src/wurf/registry.py
+++ src/wurf/registry.py
@@ -44,6 +44,7 @@ from .error import Error
 
 class RegistryProviderError(Error):
     """Exception used by the Registry"""
+
     def __init__(self, name):
         self.name = name
         super(RegistryProviderError, self).__init__(
@@ -725,7 +726,7 @@ def resolve_git(registry, ctx, options, dependency):
 @Registry.cache
 @Registry.provide
 def resolve_from_lock_git(registry, lock_cache, dependency):
-    """ Builds resolver that uses a specific checkout provided by the lock file.
+    """ Builds resolver that uses a checkout provided by the lock file.
 
     :param registry: A Registry instance.
     :param dependency: A Dependency instance.
