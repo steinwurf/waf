56e8fd05672ed032c0d17e53a3e88bf16d567803
Peter Vingelmann <petya@master.com>
2019-09-11 16:33:45 +0200

    Mark test_optional_dependencies as a networktest

--- test/optional_dependencies/test_optional_dependencies.py
+++ test/optional_dependencies/test_optional_dependencies.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
+import pytest
+
 # @todo This is actually a test that uses network, which makes it slow.
 
-
+@pytest.mark.networktest
 def test_optional_dependencies(testdirectory):
     testdirectory.copy_file('test/optional_dependencies/wscript')
     testdirectory.copy_file('build/waf')
