b34d4d243a125d0df612c36fe25911409983addd
Morten V. Pedersen <morten@mortenvp.com>
2022-02-14 11:26:30 +0100

    Allow full url for GitRepo to be specified (#119)
    * Allow full url for GitRepo to be specified

--- src/wurf/git_url_rewriter.py
+++ src/wurf/git_url_rewriter.py
@@ -14,6 +14,7 @@ class GitUrlRewriter(object):
 
     def __init__(self, parser, rewrite_protocol):
         self.parser = parser
+
         if rewrite_protocol in GitUrlRewriter.git_protocols:
             self.format_url = GitUrlRewriter.git_protocols[rewrite_protocol]
         else:
@@ -22,4 +23,9 @@ class GitUrlRewriter(object):
     def rewrite_url(self, url):
 
         u = self.parser.parse(url)
-        return self.format_url.format(host=u.host, path=u.path)
+
+        if u.protocol is None:
+            return self.format_url.format(host=u.host, path=u.path)
+        else:
+            # The user specified a full protocol so we should stick to it
+            return url
