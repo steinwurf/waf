614be8eb97028656e01da199c357ae025be058f5
Morten V. Pedersen <morten@mortenvp.com>
2022-02-21 15:41:10 +0100

    re-enable virtualenv download

--- src/wurf/virtualenv_download.py
+++ src/wurf/virtualenv_download.py
@@ -2,162 +2,69 @@
 # encoding: utf-8
 
 import os
-import sys
-import hashlib
 
-from .directory import remove_directory
+from .git import Git
 
+# This scripts downloads the standalone zipapp version of virtualenv. This is 
+# to make us independent of what is currently installed on the host OS. For 
+# example Ubunutu and Debian does not ship with default capabilities to create
+# virtual environments. 
+# We pick the version that supports Python 3.5 since that is the default
+# in the oldest Ubuntu LTS currently supported
+PYTHON_MAJOR_VERSION = 3
+PYTHON_MINOR_VERSION = 5
 
-class VirtualEnv(object):
-    """Simple object which can be used to work within a virtualenv.
+URL = "https://bootstrap.pypa.io/virtualenv/3.5/"
 
-    Example (within a wscript build function):
+class VirtualEnvDownload(object):
+    def __init__(self, ctx, log, git=None, download_path=None):
+        """Create a new VirtualEnvDownload instance
 
-    def build(bld):
-
-        venv = VirtualEnv.create(cwd='/tmp', ctx=bld, name=None)
-
-        # Will remove the virtualenv when 'with' block is exited
-        with venv:
-            venv.run('python -m pip install pytest')
-            venv.run('python -m pip --version')
-
-    It is important to be aware of the cwd parameter, e.g. if you access files
-    etc. it will be relative to cwd. So if cwd is the 'build' directory and you
-    access a file in the root of the repository it will need to be prefixed
-    with '../somefile.txt'.
-    """
-
-    def __init__(self, env, cwd, path, ctx):
+        :param ctx: A Waf context
+        :param log: A logging object
+        :param git: The Git object to use
+        :param download_path: The path where the virtualenv should be placed
         """
-        Wraps a created virtualenv
-        """
-        self.env = env
-        self.path = path
-        self.cwd = cwd
+
         self.ctx = ctx
+        self.log = log
 
-        # Make sure the virtualenv Python executable is first in PATH
-        if sys.platform == "win32":
-            python_path = os.path.join(path, "Scripts")
+        if git is None:
+            self.git = Git(git_binary="git", ctx=self.ctx)
         else:
-            python_path = os.path.join(path, "bin")
+            self.git = git
 
-        self.env["PATH"] = os.path.pathsep.join([python_path, env["PATH"]])
+        if download_path is None:
+            self.download_path = self._default_clone_path()
+        else:
+            self.download_path = download_path
 
-    def run(self, cmd, cwd=None):
-        """Runs a command in the virtualenv.
+    def download(self):
+        """Initiate the download"""
 
-        :param cmd: The command to run.
-        :param cwd: The working directory i.e. where to run the command. If not
-            specified the cwd used to create the virtual env will be used.
-        """
-        if not cwd:
-            cwd = self.cwd
+        if not os.path.isdir(self.download_path):
+            os.makedirs(self.download_path)
 
-        ret = self.ctx.exec_command(
-            cmd, cwd=cwd, env=self.env, stdout=None, stderr=None
-        )
+        repo_path = os.path.join(self.download_path, VERSION)
 
-        if ret != 0:
-            self.ctx.fatal('Exec command "{}" failed!'.format(cmd))
+        if not os.path.isdir(repo_path):
 
-    def __enter__(self):
-        """When used in a with statement the virtualenv will be automatically
-        revmoved.
-        """
-        return self
+            self.log.debug("Cloning {} into {}".format(URL, repo_path))
 
-    def __exit__(self, type, value, traceback):
-        """Remove the virtualenv."""
-        remove_directory(path=self.path)
-
-    @staticmethod
-    def create(
-        ctx,
-        log,
-        cwd=None,
-        env=None,
-        name=None,
-        overwrite=True,
-        system_site_packages=False,
-        download=True,
-        download_path=None,
-    ):
-        """Create a new virtual env.
-
-        :param ctx: The Waf Context used to run commands.
-        :param log: The logging object to use
-        :param cwd: The working directory, as a string, where the virtualenv
-            will be created and where the commands will run.
-        :param env: The environment to use during creation of the virtualenv,
-            once created the PATH and PYTHONPATH variables will be cleared to
-            reflect the virtualenv. You must make sure that the virtualenv
-            module is avilable. Either as a system package or by specifying the
-            PYTHONPATH variable.
-        :param name: The name of the virtualenv, as a string. If None a default
-            name will be used.
-        :param overwrite: If an existing virtualenv with the same name already
-            exists we will overwrite it. To reuse the virtualenv pass
-            overwrite=False.
-        :param system_site_packages: If true give the virtual environment access
-               to the global site-packages.
-        """
-
-        # The Python executable
-        python = sys.executable
-
-        if not cwd:
-            # Use the working directory of the waf context
-            cwd = ctx.path.abspath()
-
-        # Check if the cwd is a subdirectory of the build folder. It's a bad
-        # idea to create the virtualenv in the build folder. Reason being that
-        # virtualenv will create symlinks to the Python interpreter and other
-        # stuff - if those are create in the build folder waf will try to
-        # delete them when running waf clean.
-        if cwd.startswith(os.path.join(ctx.path.abspath(), "build")):
-            ctx.fatal(
-                "Cannot create virtualenv inside the build folder. "
-                "Virtualenv create symlinks to files that will be "
-                "deleted with 'waf clean'."
+            self.git.clone(
+                repository=URL,
+                directory=repo_path,
+                cwd=self.download_path,
+                depth=1,
+                branch=VERSION,
             )
 
-        if not env:
-            # Use the current environment
-            env = dict(os.environ)
+        self.log.debug("Using virtualenv from {}".format(repo_path))
 
-        # We should delete the PYTHONPATH variable if it exists. Since
-        # otherwise already installed packages might get in our way e.g.:
-        # https://stackoverflow.com/a/15887589/1717320
+        return repo_path
 
-        if "PYTHONPATH" in env:
-            del env["PYTHONPATH"]
+    def _default_clone_path(self):
 
-        if not name:
-
-            # Make a unique virtualenv for different Python executables
-            # (e.g. 2.x and 3.x)
-            unique = hashlib.sha1(python.encode("utf-8")).hexdigest()[:6]
-            name = "virtualenv-{}".format(unique)
-
-        path = os.path.join(cwd, name)
-
-        # If a virtualenv already exists - and overwrite is True
-        # lets remove it
-        if os.path.isdir(path) and overwrite:
-            remove_directory(path=path)
-
-        if os.path.isdir(path):
-            # The virtualenv already exists lets use that...
-            return VirtualEnv(env=env, path=path, cwd=cwd, ctx=ctx)
-
-        cmd = [python, "-m", "venv", name]
-
-        if system_site_packages:
-            cmd += ["--system-site-packages"]
-
-        # ctx.cmd_and_log(cmd, cwd=cwd, env=temp_env)
-        ctx.cmd_and_log(cmd, cwd=cwd, env=env)
-
-        return VirtualEnv(env=env, path=path, cwd=cwd, ctx=ctx)
+        # https://stackoverflow.com/a/4028943
+        home_path = os.path.join(os.path.expanduser("~"))
+        return os.path.join(home_path, ".waf-local-virtualenv")
